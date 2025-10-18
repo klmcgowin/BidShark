fd5c46c09c4b50431b4988c10c8732e2709df99d
01257066 <mikaelesmana27@gmail.com>
2025-10-18 16:59:56 +0800

    Enhance server.js with MongoDB connection and routes

--- Backend/server.js
+++ Backend/server.js
@@ -1,14 +1,22 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const cors = require("cors");
-const router = express.Router();
+const bcrypt = require("bcryptjs");
+
 
 dotenv.config();
 connectDB();
 
 const app = express();
 app.use(express.json());
+mongoose.connect("mongodb://127.0.0.1:27017/haida_auction", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.error("MongoDB connection error:", err));
 app.use(
     cors({
         origin: process.env.ORIGIN,
@@ -16,7 +24,9 @@ app.use(
         credentials: true,
     })
 );
-app.use(router);
+app.get("/", (req, res) => {
+    res.send("Server is running...");
+});
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/auctions", require("./routes/auctionRoutes"));
 app.use("/api/bids", require("./routes/bidRoutes"));
