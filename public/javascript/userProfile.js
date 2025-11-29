5c9b7c6d7efa9fb65336f99e51aa36c664bbcd6e
Someone <linzhewei123@gmail.com>
2025-11-29 14:22:51 +0800

    Added change profile picture

--- public/javascript/userProfile.js
+++ public/javascript/userProfile.js
@@ -1,4 +1,5 @@
 import * as sideBar from './sideBar.js';
+
 fetch('sideBar.html')
     .then(res => res.text())
     .then(html => {
@@ -40,13 +41,11 @@ document.getElementById('change').addEventListener('click', async () => {
     const name = document.getElementById('nameInput').value;
     const email = document.getElementById('emailInput').value;
     const phone = document.getElementById('numInput').value;
-    const photo = null;
     const res = await fetch('api/data/updateUserInfo', {
         method: "POST",
         credentials: "include",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
-            picture: photo,
             name: name,
             email: email,
             phoneNumber: phone,
@@ -75,3 +74,43 @@ document.getElementById('logout').addEventListener('click', async () => {
         alert(opt.message)
     }
 })
+document.getElementById('imgInput').addEventListener('change', async (e) => {
+    const file = e.target.files[0];
+    if (!file) return;
+    if (file.size > 100 * 1024 / 1.33) {
+        alert(`File too large! Max size allowed: ${(100 * 1024 / 1.33 / 1024).toFixed(1)} KB`);
+        e.target.value = "";
+        return;
+    }
+    //honestly, this is an overkill, because there is a restriction on input type but eh, better safe than sorry
+    if (file.type !== "image/png") {
+        alert("Invalid file type! Only PNG images are allowed.");
+        e.target.value = "";
+        return;
+    }
+    let img = await fileToBase64(file);
+    img = img.replace(/^data:image\/png;base64,/, "");
+    const res = await fetch('api/data/updateUserInfo', {
+        method: "POST",
+        credentials: "include",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            picture: img
+        })
+    });
+    const data = await res.json();
+    if (data.status === 'success') {
+        alert("Picture updated!");
+        window.location.reload();
+    } else {
+        alert("Update picture failed: " + (data.error || data.message));
+    }
+})
+function fileToBase64(file) {
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => resolve(reader.result);
+        reader.onerror = error => reject(error);
+    });
+}
