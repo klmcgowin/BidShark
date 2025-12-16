ef6dac3c3f81d0dabd8f8ade05f1f50c2ecc6706
Someone <linzhewei123@gmail.com>
2025-12-16 10:25:12 +0800

    shtuff

---
+++ public/javascript/chat.js
@@ -0,0 +1,18 @@
+import * as sideBar from "./sideBar.js";
+
+fetch('sideBar.html')
+    .then(res => res.text())
+    .then(html => {
+        const sidebar = document.getElementById('sidebar');
+        if (sidebar) {
+            sidebar.innerHTML = html;
+            const links = sidebar.querySelectorAll('a.nav-item');
+            const currentPage = location.pathname.split('/').pop() || 'homePage.html';
+            links.forEach(link => {
+                if (link.getAttribute('href') === currentPage) {
+                    link.classList.add('active');
+                }
+            });
+            sideBar.collapse();
+        }
+    });
